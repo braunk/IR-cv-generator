8d8da0984c896efab623c2412f2e090f0b4f4564
braunk <35941457+braunk@users.noreply.github.com>
2018-03-22 12:38:14 -0400

    remove spaes

--- cv_take5.js
+++ cv_take5.js
@@ -24,13 +24,7 @@ function buildCV(){
     var cv=("<head><title id='pagetitle'>"+ fname + " - CV </title></head>");
     
 //personal info and statement
-    cv+=("<meta name='viewport' content='width=device-width'/><meta charset='UTF-8'></head><body id='top'>
-   <div id='CV' class='instaFade'><div class='mainDetails'><div id='maininfo'><h1 class='quickFade delayTwo ' id='name'>
-" + fname + " " + localStorage['lname'] + "</h1></div><div id='contactDetails' class='quickFade delayFour'><ul><li id='email'>
-Email: " +  localStorage['email'] + "</li><li id='phone'>Phone: " + localStorage['phone'] + "</li></ul></div>
-<div class='clear'></div></div><div id='mainArea' class='quickFade delayFive'><section><article><div class='sectionTitle'>
-<h1>Personal Profile</h1></div><div class='sectionContent' id='personalprof'>" + localStorage['statement'] + "</div>
-</article><div class='clear'></div></section>");
+    cv+=("<meta name='viewport' content='width=device-width'/><meta charset='UTF-8'></head><body id='top'><div id='CV' class='instaFade'><div class='mainDetails'><div id='maininfo'><h1 class='quickFade delayTwo ' id='name'>" + fname + " " + localStorage['lname'] + "</h1></div><div id='contactDetails' class='quickFade delayFour'><ul><li id='email'>Email: " +  localStorage['email'] + "</li><li id='phone'>Phone: " + localStorage['phone'] + "</li></ul></div><div class='clear'></div></div><div id='mainArea' class='quickFade delayFive'><section><article><div class='sectionTitle'><h1>Personal Profile</h1></div><div class='sectionContent' id='personalprof'>" + localStorage['statement'] + "</div></article><div class='clear'></div></section>");
          
          //to end
          cv+=("</div></div></body>");
